73e7005b6ba20e618e741c29dca8c50bf626eae1
JosephDeFacto <josip.mareljic@gmail.com>
2024-05-09 22:15:38 +0200

    Update command.

--- src/Command/GoogleBooksCommand.php
+++ src/Command/GoogleBooksCommand.php
@@ -28,43 +28,43 @@ class GoogleBooksCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+
         $startIndex = 0;
         $maxResults = 40;
+        $endpoint = sprintf('https://www.googleapis.com/books/v1/volumes?q=subject:programming&startIndex=' . $startIndex . '&maxResults=' . $maxResults);
 
-        do {
-            $endpoint = sprintf('https://www.googleapis.com/books/v1/volumes?q=subject:programming&startIndex=' . $startIndex . '&maxResults=' . $maxResults);
+        $client = HttpClient::create();
 
-            $client = HttpClient::create();
+        $response = $client->request('GET', $endpoint);
 
-            $response = $client->request('GET', $endpoint);
+        if ($response->getStatusCode() !== 200) {
+            $output->writeln('<error>' . $response->getStatusCode() . '</error>');
+        }
 
-            if ($response->getStatusCode() !== 200) {
-                $output->writeln('<error>' . $response->getStatusCode() . '</error>');
+        $books = $response->toArray();
+
+        foreach ($books['items'] as $item) {
+            $book = new Book();
+            $book->setTitle($item['volumeInfo']['title']);
+            if (isset($item['volumeInfo']['description'])) {
+                $book->setDescription($item['volumeInfo']['description']);
             }
-
-            $books = $response->toArray();
-
-            foreach ($books['items'] as $item) {
-                $book = new Book();
-                $book->setTitle($item['volumeInfo']['title']);
-                if (is_array($item['volumeInfo']['authors'])) {
-                    foreach ($item['volumeInfo']['authors'] as $author) {
-                        $book->setAuthor($author);
-                    }
-                } else {
-                    $book->setAuthor($item['volumeInfo']['authors']);
+            if (is_array($item['volumeInfo']['authors'])) {
+                foreach ($item['volumeInfo']['authors'] as $author) {
+                    $book->setAuthor($author);
                 }
-
-                $this->entityManager->persist($book);
+            } else {
+                $book->setAuthor($item['volumeInfo']['authors']);
             }
+            $book->setPageCount($item['volumeInfo']['pageCount']);
 
-            $this->entityManager->flush();
+            $this->entityManager->persist($book);
+        }
 
-            $startIndex += $maxResults;
-        } while (count($books['items']) > 0);
+        $this->entityManager->flush();
 
-        $output->writeln('<info>Books created!</info>');
 
+        $output->writeln('<info>Books created</info>');
         return Command::SUCCESS;
     }
 }
